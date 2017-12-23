538ee208fec99c79b6ee8afc12a757b5487e01ae
pmaslankowski <pmaslankowski@gmail.com>
2017-12-23 23:33:29 +0100

    another test added

--- unit_tests.c
+++ unit_tests.c
@@ -3,10 +3,10 @@
 #include "malloc_internals.h"
 
 void allocate_one_page();
-
+void malloc_int();
 
 int main() {
-    allocate_one_page();
+    malloc_int();
     return 0;
 }
 
@@ -14,5 +14,13 @@ void allocate_one_page() {
     printf("Test: allocate_one_page:\n");
     malloc_init();
     allocate_chunk(1);
+    mdump();
+}
+
+void malloc_int() {
+    printf("Test: malloc_int:\n");    
+    int *number = (int*) foo_malloc(sizeof(int));
+    *number = 5;
+    printf("%d\n", *number);
     mdump();
 }
