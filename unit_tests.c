b649898a6ca8153b5f03b2486f0ae5c41e3311ca
pmaslankowski <pmaslankowski@gmail.com>
2017-12-29 01:24:30 +0100

    Test for block trimming added

--- unit_tests.c
+++ unit_tests.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/queue.h>
 #include "malloc.h"
 #include "malloc_internals.h"
 #include "munit.h"
 
 void allocate_chunk_test();
+void trim_block_test();
+
 void malloc_int();
 
 int main() {
     allocate_chunk_test();
+    trim_block_test();
     return 0;
 }
 
@@ -22,6 +27,28 @@ void allocate_chunk_test() {
     munit_assert_int(chunk->ma_first.mb_data[chunk->size / 8], ==, EOC);
     munit_assert_int( *(int64_t*)((void*)chunk + 4096 - 8), ==, EOC); // checks EOC in different way
     munit_assert(LIST_FIRST(&chunk->ma_freeblks) == &chunk->ma_first);
+}
+
+void trim_block_test() {
+    printf("Test: trim_block\n");
+    LIST_HEAD(, mem_block) mock_freeblks = LIST_HEAD_INITIALIZER(mock_freeblks);
+    LIST_INIT(&mock_freeblks);
+    mem_block_t* block = (mem_block_t*) aligned_alloc(32, 500);
+    LIST_INSERT_HEAD(&mock_freeblks, block, mb_node);
+
+    block->mb_size = 100;
+    set_boundary_tag(block);
+    mem_block_t* trimed_block = block_trim(block, 32);
+
+    munit_assert_int(block->mb_size, ==, 16);
+    munit_assert_int(block->mb_data[block->mb_size / 8 - 1], ==, 16); // boundary tag
+    munit_assert_int((uint64_t)trimed_block, ==, (uint64_t)&block->mb_data[2]);
+    munit_assert_int(trimed_block->mb_size, ==, 84);
+    munit_assert_int(trimed_block->mb_data[trimed_block->mb_size / 8 - 1], ==, 84);
+    munit_assert(LIST_FIRST(&mock_freeblks) == block);
+    munit_assert(LIST_NEXT(LIST_FIRST(&mock_freeblks), mb_node) == trimed_block);
+    
+    free(block);
 }
 
 void malloc_int() {
